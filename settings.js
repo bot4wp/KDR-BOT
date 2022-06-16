c4d5d0abd69d13a20cd622aabf2a50012c29cbb1
KHADHER <106883684+bot4wp@users.noreply.github.com>
2022-06-16 14:28:08 +0530

    khadh3r

--- settings.js
+++ settings.js
@@ -22,15 +22,15 @@ global.APIKeys = {
 global.owner = ['Your Number']
 global.premium = ['Your Number']
 global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ownername = 'KHADHER'
+global.botname = 'KDR-BOT'
+global.footer = '©Kdr-botInc.'
+global.ig = 'https://instagram.com/khadh3r?igshid=YmMyMTA2M2Y='
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.sc = 'https://instagram.com/khadh3r?igshid=YmMyMTA2M2Y='
+global.myweb = 'wa.me/917560920774'
+global.packname = 'KDRBOT'
+global.author = 'KHADHER'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
