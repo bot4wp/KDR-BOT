8b72b613f5fe55d0260a0ef60004fe5543c67d35
KHADHER <106883684+bot4wp@users.noreply.github.com>
2022-06-16 14:32:24 +0530

    khadh3r

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function startGojoMdNx() {
     const GojoMdNx = NexusNwIncConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Gojo Satoru\Nexus','Safari','1.0.0'],
+        browser: ['KDR-BOT/KHADHER','Safari','1.0.0'],
         auth: state
     })
 
